907d50e9bc7678e9872f437fa170b9c62c7ba400
vladspiridonov <vlad777.96@mail.ru>
2024-02-16 00:52:35 +0300

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber, secondNumber;
+            firstNumber = secondNumber = 10;
+            int sum;
+            sum = firstNumber + secondNumber;
+            Console.WriteLine("{0}+{1}={2}", firstNumber, secondNumber, sum);
+            Console.Write("f");
+            Console.ReadKey();
+        }
+    }
+}
